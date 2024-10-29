f852fc8365bbf9871b4f8c1ade730fcf2178ac8d
ciperuoy <long220352@icloud.com>
2024-10-28 22:10:17 -0400

    Add files via upload

---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace Cosmetx
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    internal class PluginInfo
+    {
+        public const string GUID = "com.ciperuoy.gorillatag.cosmetx";
+        public const string Name = "Cosmetx";
+        public const string Version = "1.0.0";
+    }
+}
